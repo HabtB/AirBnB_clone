ef1e1585e6f5f767519ce4bdc0efddccd60fe013
selma <belhadjselma1@gmail.com>
2023-02-12 16:49:54 +0100

    Define the Update command which updates instances based on the class name and id

--- console.py
+++ console.py
@@ -74,10 +74,78 @@ class HBNBCommand(cmd.Cmd):
             storage.save()
 
     def do_all(self, line):
-        """All command prints string representation of all instances"""
+        """Display string representations of all instances of a given class.
+        If no class is specified, displays all instantiated objects.
+        """
+        objdict = storage.all()
+        if line == "":
+            for key, value in objdict.items():
+                print(value)
+        elif line in storage.class_names():
+            for key, value in objdict.items():
+                if line in key:
+                    print(value)
+        else:
+            print("** class doesn't exist **")
+
+    # def do_update(self, line):
+    #     """Update command updates instances based on the class name and id"""
+    #     objdict = storage.all()
+    #     line = line.split()
+    #     if len(line) == 0:
+    #         print("** class name missing **")
+    #     elif line[0] not in storage.class_names():
+    #         print("** class doesn't exist **")
+    #     elif len(line) == 1:
+    #         print("** instance id missing **")
+    #     elif "{}.{}".format(line[0], line[1]) not in objdict.keys():
+    #         print("** no instance found **")
+    #     elif len(line) == 2:
+    #         print("** attribute name missing **")
+    #     elif len(line) == 3:
+    #         print("** value missing **")
+    #     else:
+    #         objdict["{}.{}".format(line[0], line[1])].__dict__[line[2]] = \
+    #             line[3].replace("_", " ")
+    #         storage.save()
 
     def do_update(self, line):
         """Update command updates instances based on the class name and id"""
+        objdict = storage.all()
+        line = line.split()
+        if len(line) == 0:
+            print("** class name missing **")
+        elif line[0] not in storage.class_names():
+            print("** class doesn't exist **")
+        elif len(line) == 1:
+            print("** instance id missing **")
+        elif "{}.{}".format(line[0], line[1]) not in objdict.keys():
+            print("** no instance found **")
+        elif len(line) == 2:
+            print("** attribute name missing **")
+        elif len(line) == 3:
+            print("** value missing **")
+        elif line[2] in ['id', 'created_at', 'updated_at']:
+            print("** attribute name can't be updated **")
+        elif len(line) > 4:
+            print("** too many arguments **")
+        else:
+            instance = objdict["{}.{}".format(line[0], line[1])]
+            try:
+                # Check if the value is of string type
+                if line[3].startswith('"') and line[3].endswith('"'):
+                    value = line[3][1:-1]
+                # Check if the value is of integer type
+                elif line[3].isdigit():
+                    value = int(line[3])
+                # Check if the value is of float type
+                else:
+                    value = float(line[3])
+            except ValueError:
+                print("** value is not a valid type **")
+            else:
+                setattr(instance, line[2], value)
+                instance.save()
 
 
 if __name__ == "__main__":
