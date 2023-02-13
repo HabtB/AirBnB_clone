ac85a94a642088d164a14cafb4901eb87872badf
HabtB <email.Habtamu@gmail.com>
2023-02-13 17:21:18 +0000

    base_model updated

--- models/base_model.py
+++ models/base_model.py
@@ -23,8 +23,8 @@ class BaseModel:
                     self.__dict__[key] = value
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
             storage.new(self)
 
     def __str__(self):
@@ -32,7 +32,7 @@ class BaseModel:
 
     def save(self):
         """ updates the time when it object gets updated """
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.utcnow()
         storage.save()
 
     def to_dict(self):
