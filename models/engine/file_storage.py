7fd69cf9f17b576798451c535209799c86d04274
HabtB <email.Habtamu@gmail.com>
2023-02-13 06:14:59 +0000

    file_storage updated

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -60,3 +60,42 @@ class FileStorage:
             obj_dict = {key: self.class_names()[value["__class__"]](**value)
                         for key, value in obj_dict.items()}
             FileStorage.__objects = obj_dict
+
+        def attributes(self):
+            """Returns the valid attributes and their types for classname"""
+
+            attributes = {
+                "BaseModel": {
+                        "id": str,
+                        "created_at": datetime.datetime,
+                        "updated_at": datetime.datetime},
+                "User":
+                    {"email": str,
+                     "password": str,
+                     "first_name": str,
+                     "last_name": str},
+                "State":
+                    {"name": str},
+                "City":
+                    {"state_id": str,
+                     "name": str},
+                "Amenity":
+                    {"name": str},
+                "Place":
+                    {"city_id": str,
+                     "user_id": str,
+                     "name": str,
+                     "description": str,
+                     "number_rooms": int,
+                     "number_bathrooms": int,
+                     "max_guest": int,
+                     "price_by_night": int,
+                     "latitude": float,
+                     "longitude": float,
+                     "amenity_ids": list},
+                "Review":
+                    {"place_id": str,
+                     "user_id": str,
+                     "text": str}
+            }
+            return attributes
